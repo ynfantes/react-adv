c076ae9d1cb6a9edd30f6ff5b4ec02703898fa41
Edgar Messia <ynfantes@gmail.com>
2022-12-08 18:29:41 +0100

    Control props alternativo listo

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -10,14 +10,8 @@ interface useProductArgs {
 export const useProduct = ( { onChange, product, value = 0 } : useProductArgs ) => {
     const [ counter, setCounter] = useState( value )
     
-    const isControled = useRef( !!onChange )
-
     const increaseBy = ( value: number ) => {
-
-        if ( isControled.current ) {
-            return onChange!({ count: value, product })
-        }
-
+    
         const newValue = Math.max( counter + value, 0)
         setCounter( newValue)
         onChange && onChange({ count: newValue, product })
--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -10,22 +10,16 @@ export const useShoppingCart = () => {
     
         setShoppingCart( oldShoppingCart  => {
             
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
+            if( count === 0) {
 
-            if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
-            
-                productInCart.count += count;
-
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart
+                return rest
             }
 
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
-
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
+            }
         })
     }
     return {
